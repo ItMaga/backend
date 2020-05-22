dc4f27121effb7cb3e4e48a12b982d12ef6238bf
ItMaga <51162910+ItMaga@users.noreply.github.com>
2020-05-22 16:00:29 +0300

    отметки звонков

--- routes/callback.js
+++ routes/callback.js
@@ -30,4 +30,23 @@ router.get('/get', function(req, res, next) {
     });
 });
 
+/* PUT Изменение отметки звонков*/
+router.put('/change', function(req, res, next) {
+    var id = req.get("Id");
+
+    var select = req.body.select;
+
+    connection.query('UPDATE call_back SET call_back.select = ? WHERE id_call_back = ?',
+        [select, id], function(error, results, fields) {
+            if (error) {
+                console.log('error');
+                return res.send(JSON.stringify({"status": 500, "error": error, "response": null}));
+            } else {
+                console.log('success');
+                res.send(JSON.stringify({"status": 200, "error": null, "response": results}));
+            }
+        })
+
+});
+
 module.exports = router;
